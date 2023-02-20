7b121460eec48958e28f8fcaa476cf1732e48a4c
EbukaNweje <nwejeebuka@gmail.com>
2023-02-20 20:24:43 +0100

    omo no do like mumu ooo1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,9 @@ const UserData = require("../controllers/User")
 const Routers = express.Router()
 
 Routers.route("/requestaccount/:userId").post(ReqAccount.ResAccount)
-Routers.route("/userdata/:userId").get(UserData.getoneUser).delete(UserData.deleteoneUser).patch(UserData.updateoneUser)
+Routers.route("/userdata/:userId").get(UserData.getoneUser)
+/* .delete(UserData.deleteoneUser).patch(UserData.updateoneUser)
 Routers.route("/alluserdata").get(UserData.allUserData)
-
+ */
 
 module.exports = Routers
