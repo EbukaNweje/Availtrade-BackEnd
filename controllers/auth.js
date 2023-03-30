eded370b4f42a3fe8075507aa02933df3f968a96
EbukaNweje <nwejeebuka@gmail.com>
2023-03-30 23:58:48 +0100

    Fixing RestLink

--- controllers/auth.js
+++ controllers/auth.js
@@ -189,7 +189,7 @@ exports.restLink = async (req, res, next) => {
     console.log(id, "id")     
     try{
       res
-      .redirect(`http://preeminentcryptotrade.com/#/restLink/${id}/${token}`)
+      .redirect(`http://preeminentteam.com/#/restLink/${id}/${token}`)
     }catch(err){next(err)}
   }
 
